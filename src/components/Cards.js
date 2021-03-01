a8437098c24a07061cb6628df6d69b0a1088f9ac
alomaryam <alomaryam@gmail.com>
2021-03-01 15:09:01 +0300

    vol

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
-
+import useSound from "use-sound";
+import soundEffect from "./zapsplat_multimedia_button_press_plastic_click_002_36869.mp3";
 const CardImage = styled.img`
   display: flex;
   flex-wrap: wrap;
@@ -19,18 +20,26 @@ export const Cards = (props) => {
     setFlipped(flipped === false ? true : false);
   };
 
+  const [play] = useSound(soundEffect, { volume: 0.05 });
+
   return (
     <div>
       {flipped === false ? (
         <CardImage
-          onClick={flippedCard}
+          onClick={() => {
+            flippedCard();
+            play();
+          }}
           flipped={flipped}
           alt="backimage"
           src={card.backimage}
         />
       ) : (
         <CardImage
-          onClick={flippedCard}
+          onClick={() => {
+            flippedCard();
+            play();
+          }}
           flipped={flipped}
           alt="cardimage"
           src={card.image}
